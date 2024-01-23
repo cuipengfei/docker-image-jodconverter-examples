909d9c62859b9c55b49afca0c7df7e04546042cc
cuipengfei <cuipengfei2008@gmail.com>
2024-01-23 16:45:54 +0800

    update su -c command

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -5,5 +5,5 @@ if [ "$1" = "java" ]; then
 elif [ "$1" = "./gradlew" ]; then
     exec "$@"
 else
-    exec su jodconverter -c java -Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8 -jar ${JAR_FILE_BASEDIR}/${JAR_FILE_NAME} "$@" > >(tee -a ${LOG_BASE_DIR}/app.log) 2> >(tee -a ${LOG_BASE_DIR}/app.err >&2)
+    exec su jodconverter -c 'java -Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8 -jar ${JAR_FILE_BASEDIR}/${JAR_FILE_NAME} "$@"' > >(tee -a ${LOG_BASE_DIR}/app.log) 2> >(tee -a ${LOG_BASE_DIR}/app.err >&2)
 fi
