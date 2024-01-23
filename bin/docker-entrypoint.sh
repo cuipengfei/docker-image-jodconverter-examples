ac54ee9dac0e927fdff1d7a0ccdfc1675732b9ee
崔鹏飞 Cui Pengfei <cuipengfei2008@gmail.com>
2024-01-23 15:29:05 +0800

    Update docker-entrypoint.sh

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 if [ "$1" = "java" ]; then
     # the case where the user uses his own java -jar startup command - let him do that
-    exec gosu jodconverter "$@" > >(tee -a ${LOG_BASE_DIR}/app.log) 2> >(tee -a ${LOG_BASE_DIR}/app.err >&2)
+    exec su jodconverter -c "$@" > >(tee -a ${LOG_BASE_DIR}/app.log) 2> >(tee -a ${LOG_BASE_DIR}/app.err >&2)
 elif [ "$1" = "./gradlew" ]; then
     exec "$@"
 else
-    exec gosu jodconverter java -Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8 -jar ${JAR_FILE_BASEDIR}/${JAR_FILE_NAME} "$@" > >(tee -a ${LOG_BASE_DIR}/app.log) 2> >(tee -a ${LOG_BASE_DIR}/app.err >&2)
+    exec su jodconverter -c java -Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8 -jar ${JAR_FILE_BASEDIR}/${JAR_FILE_NAME} "$@" > >(tee -a ${LOG_BASE_DIR}/app.log) 2> >(tee -a ${LOG_BASE_DIR}/app.err >&2)
 fi
